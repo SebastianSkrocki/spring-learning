eef638e24a931b30aa85a22143dbab972979a8ee
SSkrocki <seb.skrocki@gmail.com>
2022-08-08 21:25:05 +0200

    Basic validation and deleting rows from table

--- src/main/java/com/sebastianskrocki/springlearning/biz/model/Person.java
+++ src/main/java/com/sebastianskrocki/springlearning/biz/model/Person.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -18,9 +19,22 @@ import java.time.LocalDate;
         @Id
         @GeneratedValue
         private Long id;
+
+        @NotEmpty(message = "First name can not be empty")
         private String firstName;
+
+        @NotEmpty(message = "Last name can not be empty")
         private String lastName;
+
+        @Past(message = "Date of birth must be in the past")
+        @NotNull(message = "Date of birth can not be empty")
         private LocalDate dob;
+
+        @Email(message = "Email must be valid")
+        @NotEmpty(message = "Email can not be empty")
         private String email;
+
+        @DecimalMin(value = "1000.00", message = "Salary must be at least 1000")
+        @NotNull(message = "Salary must be specified")
         private BigDecimal salary;
     }
--- src/main/java/com/sebastianskrocki/springlearning/web/controller/PeopleController.java
+++ src/main/java/com/sebastianskrocki/springlearning/web/controller/PeopleController.java
@@ -3,7 +3,12 @@ package com.sebastianskrocki.springlearning.web.controller;
 import com.sebastianskrocki.springlearning.biz.model.Person;
 import com.sebastianskrocki.springlearning.data.PersonRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/people")
@@ -29,13 +34,17 @@ public class PeopleController {
         return "people";
     }
     @PostMapping
-    public String savePerson(Person person){
-        personRepository.save(person);
-        return "redirect:people";
+    public String savePerson(@Valid Person person, Errors errors){
+        if (!errors.hasErrors()) {
+            personRepository.save(person);
+            return "redirect:people";
+        }
+        return "people";
     }
-    @DeleteMapping
-    public String deletePerson(Person person){
-        personRepository.delete(person);
+    @PostMapping(params = "delete=true")
+    public String deletePeople(@RequestParam Optional<List<Long>> selections){
+        System.out.println(selections);
+        selections.ifPresent(personRepository::deleteAllById);
         return "redirect:people";
     }
 }
